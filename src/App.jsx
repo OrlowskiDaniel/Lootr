10c09d99edef53bfe5fb9c9eed6ac66c509804e2
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-05-12 06:28:39 +0200

    supabase connection + test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { supabase } from "./lib/supabaseClient";
+
 function App() {
+
+  useEffect(() => {
+    const testConnection = async () => {
+      const { data, error } = await supabase.from('test').select('*');
+      console.log(data, error);
+    };
+
+    testConnection();
+  }, []);
+
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold">Lootr 🚀</h1>
+    <div className="text-white bg-gray-900 h-screen flex items-center justify-center">
+      Lootr connected 🔥
     </div>
   );
 }
