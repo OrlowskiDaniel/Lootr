be51dda7c5896f256ae91386571102fc22fe4495
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-05-13 14:58:56 +0200

    auth api with supabase functions
    - supabase functions used for sign up, sign in and sign out
    - for each function error handling message

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,15 +2,30 @@ import { supabase } from "../lib/supabaseClient";
 
 // SIGN UP
 export async function signUp(email, password) {
-
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+    });
+    if (error) {
+        console.error("Error signing up:", error.message);
+    }
 }
 
 // SIGN IN
 export async function signIn(email, password) {
-
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+    if (error) {
+        console.error("Error signing in:", error.message);
+    }
 }
 
 // SIGN OUT
 export async function signOut() {
-
+    const { error } = await supabase.auth.signOut(); 
+    if (error) {
+        console.error("Error signing out:", error.message);
+    }
 }
