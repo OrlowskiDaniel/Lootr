e5c3b0d1e4990e6a67fe750d1debc9e20b5b32b9
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-05-13 14:50:29 +0200

    register form setup + handle func

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,36 @@
 import { useState } from "react";
+import { signUp } from "../api/auth";
 
 export default function Register() {
     // login consts
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     // handle submit function
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // call signUp function from auth.js
+        await signUp(email, password);
+
+    };
 
     return (
-        <form>
-            <button>Register</button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 }
